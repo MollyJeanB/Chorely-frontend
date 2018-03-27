f1084d379b69e090d69b9d11d1b8e03b7606512e
Molly Jean Bennett <mollyjeanbennett@gmail.com>
2018-03-27 14:02:11 -0700

    remove logo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Link, Route } from "react-router-dom";
 import { Switch } from "react-router";
