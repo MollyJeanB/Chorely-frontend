c45030776111feeaed4146898575b824b412fcb8
Molly Jean Bennett <mollyjeanbennett@gmail.com>
2018-04-12 19:41:56 -0700

    mock up form submit for new chore form

--- src/components/ChoreForm.js
+++ src/components/ChoreForm.js
@@ -3,20 +3,52 @@ import styles from "../componentStyles/ChoreForm.css";
 import { connect } from "react-redux";
 
 export class ChoreForm extends React.Component {
-  // onSubmit(values) {
-  //   if (values.choreTitle) {
-  //     values.choreTitle = this.props.choreTitle;
-  //     this.props.dispatch(submitNewChore(values));
-  //   } else {
-  //     return;
-  //   }
-  // }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      choreName: "",
+      pointValueField: 1,
+      timesPerWeekField: 1,
+    }
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleName(event) {
+    this.setState({
+      choreName: event.target.value
+    })
+  }
+
+  handlePointValue(event) {
+    this.setState({
+      pointValueField: event.target.value
+    })
+  }
+
+  handleTimesPerWeek(event) {
+    this.setState({
+      timesPerWeekField: event.target.value
+    })
+  }
+
+
+  handleSubmit(event) {
+    event.preventDefault()
+    console.log(this.state)
+    this.props.toggleForm()
+    this.setState = {
+      choreName: "",
+      pointValueField: 1,
+      timesPerWeekField: 1,
+    }
+  }
 
   render() {
     return (
       <form
         className={styles.formBox}
-        // onSubmit={this.props.handleSubmit(this.onSubmit.bind(this))}
+        onSubmit={this.handleSubmit}
       >
         <input
           name="choreName"
@@ -24,9 +56,15 @@ export class ChoreForm extends React.Component {
           ref={input => (this.textInput = input)}
           placeholder="Chore Title"
           className={styles.choreTitle}
+          value={this.state.value}
+          onChange={e => this.handleName(e)}
         />
         <div className={styles.pointContain}>
-          <select name="pointValueField" className={styles.pointField}>
+          <select
+            name="pointValueField"
+            className={styles.pointField}
+            onChange={e => this.handlePointValue(e)}
+            >
             <option value="1">Point Value</option>
             <option value="1">1</option>
             <option value="2">2</option>
@@ -41,7 +79,11 @@ export class ChoreForm extends React.Component {
           </select>
         </div>
         <div className={styles.weekContain}>
-          <select name="timesPerWeekField" className={styles.weekField}>
+          <select
+            name="timesPerWeekField"
+            className={styles.weekField}
+            onChange={e => this.handleTimesPerWeek(e)}
+            >
             <option value="1">Times Per Week</option>
             <option value="1">1</option>
             <option value="2">2</option>
