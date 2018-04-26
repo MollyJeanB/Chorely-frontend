5ab9f1bd1e5e905225d27bc0a25152e26d5cde3a
Lawrence Whiteside <law@cinemasetfree.com>
2018-04-26 12:36:14 -0700

    quick fix

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -2,12 +2,11 @@ import React from "react"
 import styles from "../componentStyles/Chart.css"
 import MemberList from "./MemberList"
 import ChoreList from "./ChoreList"
-import {connect } from "react-redux"
-import {getChartData} from "../actions/actions"
+import { connect } from "react-redux"
+import { getChartData } from "../actions/actions"
 // import Spinner from "react-spinkit"
 
 class Chart extends React.Component {
-
   constructor(props) {
     super(props)
     this.state = {
@@ -15,44 +14,40 @@ class Chart extends React.Component {
     }
   }
 
-componentDidMount() {
-  this.props.dispatch(getChartData())
-  this.setState({
-    loading: false
-  })
-}
-
-  render() {
-
-
- let spinner;
- let memberSection;
- let choreSection;
- if (this.state.loading) {
-   spinner = <div className={styles.loadTest}>LOAD</div>
-   // <Spinner name="double-bounce" />
- }
-
- if (!this.state.loading) {
-   memberSection = <MemberList {...this.props} />
-   choreSection =   <ChoreList {...this.props} />
-
- }
-
-
-    return   (
-        <div className={styles.pageContainer}>
-          {spinner}
-          {memberSection}
-          {choreSection}
-        </div>
-      )
+  componentDidMount() {
+    this.props.dispatch(getChartData())
+    this.setState({
+      loading: false
+    })
   }
 
+  render() {
+    let spinner
+    let memberSection
+    let choreSection
+    if (this.state.loading) {
+      spinner = <div className={styles.loadTest}>LOAD</div>
+      // <Spinner name="double-bounce" />
+    }
+
+    if (!this.state.loading) {
+      memberSection = <MemberList {...this.props} />
+      choreSection = <ChoreList {...this.props} />
+    }
+
+    return (
+      <div className={styles.pageContainer}>
+        {spinner}
+        {memberSection}
+        {choreSection}
+      </div>
+    )
+  }
 }
 
-export default connect (state => ({
+export default connect(state => ({
   chores: state.chores,
   members: state.members,
-  completions: state.completions
+  completions: state.completions,
+  updated: new Date()
 }))(Chart)
--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -2,7 +2,7 @@ import {
   EXPAND_MENU,
   CHANGE_COLOR,
   GET_CHART_DATA_SUCCESS
-} from "../actions/actions";
+} from "../actions/actions"
 
 import {
   POST_MEMBER_SUCCESS,
@@ -20,90 +20,102 @@ const initialState = {
   resetTime: "Sunday at 5pm",
   members: {},
   chores: {},
-  completions: {},
-};
+  completions: {}
+}
 
 export const choreReducer = (state = initialState, action) => {
   if (action.type === EXPAND_MENU) {
     return Object.assign({}, state, {
       drawerOpen: !state.drawerOpen
-    });
-  }
-
-
-  if (action.type === DELETE_CHORE_SUCCESS) {
-    let choreArray = [...state.chores]
-    let deletedIndex = choreArray.findIndex(item => item.id === action.id);
-   choreArray.splice(deletedIndex, 1);
-       return Object.assign({}, state, {
-         chores: choreArray
-       });
-  }
-
-  if (action.type === DELETE_MEMBER_SUCCESS) {
-    let memberArray = [...state.members]
-    let deletedIndex = memberArray.findIndex(item => item.id === action.id);
-   memberArray.splice(deletedIndex, 1);
-       return Object.assign({}, state, {
-         members: memberArray
-       });
-  }
-
-if (action.type === POST_MEMBER_SUCCESS) {
-  return Object.assign({}, state, {
-    members: [action.values, ...state.members]
-  })
-}
-
-if (action.type === POST_CHORE_SUCCESS) {
-  return Object.assign({}, state, {
-    chores: [action.values, ...state.chores]
-  })
-}
-
-if (action.type === UPDATE_CHORE_SUCCESS) {
-  let choreArray = [...state.chores]
-  choreArray.forEach(chore => {
-  if (chore.id === action.values.id) {
-    console.log(chore)
-    chore = Object.assign(chore, action.values)
-  }
-})
-  return Object.assign({}, state, {
-    chores: choreArray
-  })
-}
-
-if (action.type === UPDATE_MEMBER_SUCCESS) {
-  let memberArray = [...state.members]
-  memberArray.forEach(member => {
-  if (member.id === action.values.id) {
-    member = Object.assign(member, action.values)
-  }
-})
-  return Object.assign({}, state, {
-    members: memberArray
-  })
-}
-
-if (action.type === CHANGE_COLOR) {
-  let memberArray = [...state.members]
-  memberArray.forEach(member => {
-    if (member.id === action.id) {
-      member.color = action.color;
-    }
-  })
-  return Object.assign({}, state, {
-    members: memberArray
-  });
-}
-
-  if (action.type === GET_CHART_DATA_SUCCESS) {
-    return Object.assign({}, state, {
-      members: action.values.members,
-      chores: action.values.chores
     })
   }
 
-  return state;
-};
+  if (action.type === DELETE_CHORE_SUCCESS) {
+    let newChores = state.members
+    delete newChores[action.id]
+    return Object.assign({}, state, {
+      members: newChores
+    })
+  }
+
+  if (action.type === DELETE_MEMBER_SUCCESS) {
+    let newMembers = state.members
+    delete newMembers[action.id]
+    return Object.assign({}, state, {
+      members: newMembers
+    })
+  }
+
+  if (action.type === POST_MEMBER_SUCCESS) {
+    let newMembers = state.members
+    newMembers[action.values.id] = action.values
+    return Object.assign({}, state, {
+      members: newMembers
+    })
+  }
+
+  if (action.type === POST_CHORE_SUCCESS) {
+    let newChores = state.chores
+    newChores[action.values.id] = action.values
+    return Object.assign({}, state, {
+      chores: newChores
+    })
+  }
+
+  if (action.type === UPDATE_CHORE_SUCCESS) {
+    let choreKeys = Object.keys(state.chores)
+    let newChores = state.chores
+    choreKeys.forEach(key => {
+      let chore = state.chores[key]
+      if (key === action.values.id) {
+        chore = Object.assign(chore, action.values)
+      }
+    })
+    return Object.assign({}, state, {
+      chores: newChores
+    })
+  }
+
+  if (action.type === UPDATE_MEMBER_SUCCESS) {
+    let memberKeys = Object.keys(state.members)
+    let newMembers = state.members
+    memberKeys.forEach(key => {
+      let member = state.members[key]
+      if (key === action.values.id) {
+        newMembers[key] = Object.assign(member, action.values)
+      }
+    })
+    return Object.assign({}, state, {
+      members: newMembers
+    })
+  }
+
+  if (action.type === CHANGE_COLOR) {
+    let memberKeys = Object.keys(state.members)
+    let newMembers = state.members
+    memberKeys.forEach(key => {
+      let member = state.members[key]
+      if (key === action.id) {
+        member.color = action.color
+      }
+    })
+    return Object.assign({}, state, {
+      members: newMembers
+    })
+  }
+
+  if (action.type === GET_CHART_DATA_SUCCESS) {
+    return Object.assign({}, state, {
+      members: action.values.members.reduce((acc, cur) => {
+        acc[cur.id] = cur
+        return acc
+      }, {}),
+      chores: action.values.chores.reduce((acc, cur) => {
+        acc[cur.id] = cur
+        return acc
+      }, {})
+    })
+  }
+
+  return state
+}
